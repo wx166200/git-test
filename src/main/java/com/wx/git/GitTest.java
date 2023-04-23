95183e91e8f8c7f229fc4670e5f4e217d681b4e4
George <George@wx.com>
2023-04-23 17:21:07 +0800

    second commit

--- src/main/java/com/wx/git/GitTest.java
+++ src/main/java/com/wx/git/GitTest.java
@@ -16,5 +16,6 @@ package com.wx.git;/**
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
