b48e47d191268bb8f49236df4372c7df8fabb880
George <George@wx.com>
2023-04-23 17:09:36 +0800

    first commit

---
+++ src/main/java/com/wx/git/GitTest.java
@@ -0,0 +1,20 @@
+package com.wx.git;/**
+ * @author wxstart
+ * @create 2023-04-23 16:54
+ */
+
+/**
+ * @program: git-test
+ *
+ * @description:
+ *
+ * @author: Mr.Wei
+ *
+ * @create: 2023-04-23 16:54
+ **/
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
